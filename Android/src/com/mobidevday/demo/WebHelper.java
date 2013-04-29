605e5f0d74d3b7c49aedc24b7a198c892b5acf3c
David Truxall <davetrux@gmail.com>
2013-04-28 20:50:10 -0400

    Bug fixes, icon

--- Android/src/com/mobidevday/demo/WebHelper.java
+++ Android/src/com/mobidevday/demo/WebHelper.java
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Base64;
 import com.mobidevday.demo.ntlm.NTLMSchemeFactory;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
@@ -12,11 +13,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.util.InetAddressUtils;
 import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -84,24 +83,7 @@ public class WebHelper {
         return json;
     }
 
-//    public String getHttp(String url, String token) throws IOException {
-//
-//        //Set up the HTTP calls
-//        HttpGet request = new HttpGet(url);
-//        request.addHeader("Authorization", String.format("Bearer {0}", token));
-//
-//        HttpResponse response = mClient.execute(request);
-//
-//        //Get the data from the body of the response
-//        InputStream stream = response.getEntity().getContent();
-//     	byte byteArray[] = IOUtils.toByteArray(stream);
-//     	String json = new String( byteArray );
-//     	stream.close();
-//        return json;
-//    }
-
     public String getHttp(String url, String user, String password) throws IOException {
-
         //Set up the HTTP calls
         HttpGet request = new HttpGet(url);
 
@@ -120,11 +102,11 @@ public class WebHelper {
     }
 
     private String createBasicHeader(String userName, String password) {
-        String combined = String.format("{0}:{1}", userName, password);
+        String combined = String.format("%s:%s", userName, password);
 
-        String b64 =  Base64.encodeToString(combined.getBytes(), Base64.DEFAULT);
+        String b64 =  Base64.encodeToString(combined.getBytes(), Base64.NO_WRAP);
 
-        return String.format("Basic {0}", b64);
+        return String.format("Basic %s", b64);
     }
 
     public String getLocalIpAddress()
